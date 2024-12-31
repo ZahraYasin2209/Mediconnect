503539af310beabd49fa63280c7dcbee3669e1b3
Zahra Yasin <109916495+ZahraYasin2209@users.noreply.github.com>
2024-12-31 18:23:24 +0500

    Add files via upload

---
+++ mediconnect.sql
@@ -0,0 +1,105 @@
+/*
+SQLyog Ultimate - MySQL GUI v8.21 
+MySQL - 5.1.29-rc-community : Database - mediconnect
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`mediconnect` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `mediconnect`;
+
+/*Table structure for table `appointments` */
+
+DROP TABLE IF EXISTS `appointments`;
+
+CREATE TABLE `appointments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `patient_id` int(11) DEFAULT NULL,
+  `doctor_id` int(11) DEFAULT NULL,
+  `appointment_date` date DEFAULT NULL,
+  `appointment_time` time DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `patient_id` (`patient_id`),
+  KEY `doctor_id` (`doctor_id`),
+  CONSTRAINT `appointments_ibfk_1` FOREIGN KEY (`patient_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `appointments_ibfk_2` FOREIGN KEY (`doctor_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+/*Data for the table `appointments` */
+
+insert  into `appointments`(`id`,`patient_id`,`doctor_id`,`appointment_date`,`appointment_time`,`description`) values (1,3,2,'2024-06-25','22:45:00','Fever'),(3,5,2,'2024-06-26','22:50:00','Acne');
+
+/*Table structure for table `patients` */
+
+DROP TABLE IF EXISTS `patients`;
+
+CREATE TABLE `patients` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `name` varchar(50) NOT NULL,
+  `age` int(11) DEFAULT NULL,
+  `gender` varchar(10) DEFAULT NULL,
+  `contact` varchar(15) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `patients_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+/*Data for the table `patients` */
+
+insert  into `patients`(`id`,`user_id`,`name`,`age`,`gender`,`contact`,`address`,`email`) values (1,1,'Hareem Fatima',20,'Female','+92-34698765432','Lahore','hareem@gmail.com'),(2,3,'Muhammad Hamza',25,'Male','+92-3001234567','Karachi','hamza@gmail.com'),(3,5,'Zahra Yasin',20,'Female','+92-3461234567','Lahore Cantt','zahra@gmail.com');
+
+/*Table structure for table `reports` */
+
+DROP TABLE IF EXISTS `reports`;
+
+CREATE TABLE `reports` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `patient_id` int(11) DEFAULT NULL,
+  `doctor_id` int(11) DEFAULT NULL,
+  `report_date` date DEFAULT NULL,
+  `description` text,
+  PRIMARY KEY (`id`),
+  KEY `patient_id` (`patient_id`),
+  KEY `doctor_id` (`doctor_id`),
+  CONSTRAINT `reports_ibfk_1` FOREIGN KEY (`patient_id`) REFERENCES `patients` (`id`),
+  CONSTRAINT `reports_ibfk_2` FOREIGN KEY (`doctor_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `reports` */
+
+insert  into `reports`(`id`,`patient_id`,`doctor_id`,`report_date`,`description`) values (2,3,2,'2024-06-26','Acne problem. Medicine prescribed.\r\nFollow up after two weeks');
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `role` enum('patient','doctor','admin') NOT NULL,
+  `email` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`username`,`password`,`role`,`email`) values (1,'Hareem Fatima','hareem','patient','hareem@gmail.com'),(2,'Ahmed Mustafa','ahmed','doctor','ahmed@gmail.com'),(3,'Muhammad Hamza','hamza','patient','hamza@gmail.com'),(4,'Momina Nawaz','momina','admin','momina@gmail.com'),(5,'Zahra Yasin','zahra','patient','zahra@gmail.com'),(6,'Asad','asad','patient','asad@gmail.com');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
